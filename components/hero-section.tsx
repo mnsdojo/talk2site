29c1fc9e49e72d198e404ebaea895e12a378a3cf
sensei <manishkumar97430@gmail.com>
2024-10-01 13:47:51 -0400

    updated readme

---
+++ components/hero-section.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+import { useState } from "react";
+
+function HeroSection() {
+  const [url, setUrl] = useState("");
+  return <div>HeroSection</div>;
+}
+
+export default HeroSection;
